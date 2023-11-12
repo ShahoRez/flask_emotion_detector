ac059af333f5d74e899debfbfcb3ad16079d9850
Shaho Rezaei <shahorezaei@gmail.com>
2023-11-12 11:17:47 -0500

    Convert the response text into a dictionary using the json library functions.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -7,5 +7,11 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    
+    formatted_response = formatted_response['emotionPredictions'][0]['emotion']
+    emotions_list = list(formatted_response.keys())
+    scores_list = list(formatted_response.values())
+    dominant_emotion_score = max(scores_list)
+    ind = scores_list.index(dominant_emotion_score)
+    dominant_emotion = emotions_list[ind]
+    formatted_response['dominant_emotion'] = dominant_emotion
     return formatted_response

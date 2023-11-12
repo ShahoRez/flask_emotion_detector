a7dbb0844e3e2adcb890a061717d4d4851605f84
Shaho Rezaei <shahorezaei@gmail.com>
2023-11-12 12:25:34 -0500

    error handeling in emotion detetcor fro balnk enteries

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,12 +6,16 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    formatted_response = formatted_response['emotionPredictions'][0]['emotion']
-    emotions_list = list(formatted_response.keys())
-    scores_list = list(formatted_response.values())
-    dominant_emotion_score = max(scores_list)
-    ind = scores_list.index(dominant_emotion_score)
-    dominant_emotion = emotions_list[ind]
-    formatted_response['dominant_emotion'] = dominant_emotion
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        formatted_response = formatted_response['emotionPredictions'][0]['emotion']
+        emotions_list = list(formatted_response.keys())
+        scores_list = list(formatted_response.values())
+        dominant_emotion_score = max(scores_list)
+        ind = scores_list.index(dominant_emotion_score)
+        dominant_emotion = emotions_list[ind]
+        formatted_response['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400:
+        formatted_response = None
+    
     return formatted_response
